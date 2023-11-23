ba4c413592a6c6ebb63538c0d06e7620e586497c
Irina Meroi <irimeroi7@gmail.com>
2023-11-23 12:08:15 -0800

    added basics

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const route1 = require('./addroutenamehere');
+// const route2 = require('./addroutenamehere');
+
+// router.use('/route1', route1);
+// router.use('/route2', route2);
+
+module.exports = router;
